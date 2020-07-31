9667738c3dd7289f06ed76c20e10ad81da0c1d2d
lianekai <1064108741@qq.com>
2020-08-01 01:08:35 +0800

    2020/08/01第一次建好项目，为以后写一个个人博客开了头，加油，你可以的！！！！lianekai

---
+++ src/main/java/com/lianekai/myblog/MyBlogApplication.java
@@ -0,0 +1,15 @@
+package com.lianekai.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lianekai
+ * 于2020年8月1日开始，决定写一个基于SpringBoot框架的个人博客平台，当做自身技术学习成长的项目，加油，赶紧向计算机大牛迈向步伐吧。。
+ */
+@SpringBootApplication
+public class MyBlogApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MyBlogApplication.class, args);
+    }
+}
